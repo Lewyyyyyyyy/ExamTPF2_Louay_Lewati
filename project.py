076f7ac48866d9a41ef4c0565db3f72980c4f169
Louay Lewati <louaylw@gmail.com>
2025-03-13 10:32:02 +0100

    new py content

--- project.py
+++ project.py
@@ -1,9 +1,6 @@
-def somme(T):
-    S=0
-    for t in T:
-        S+=t
-        return S
 
 Data=[1,2,5]
-Som=Sum(Data)
-print('la somme est :',som)
+print('la somme est :',sum(Data))
+print('max:',max(Data))
+print('min:',min(Data))
+
