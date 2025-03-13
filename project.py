56f687a6ebe5bf5e4934ec139797553f1fb5c0b4
Louay Lewati <louaylw@gmail.com>
2025-03-13 10:34:23 +0100

    new py content

--- project.py
+++ project.py
@@ -1,6 +1,8 @@
 
 Data=[1,2,5]
-print('la somme est :',sum(Data))
-print('max:',max(Data))
-print('min:',min(Data))
-
+if Data :
+    print('la somme est :',sum(Data))
+    print('max:',max(Data))
+    print('min:',min(Data))
+else :
+    print('vide')
